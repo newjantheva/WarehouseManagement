bd66e927ffd28a5b943fe41f9f2f6a924ecc5b3a
newjantheva <newjant@gmail.com>
2020-12-01 11:35:27 +0100

    Add project files.

---
+++ WarehouseManagement.Core/Sequences/SequenceService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseManagement.Core.Sequences
+{
+    interface ISequenceService
+    {
+
+    }
+
+    public class SequenceService : ISequenceService
+    {
+
+    }
+}
---
+++ WarehouseManagement.Core/Terminals/TerminalService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseManagement.Core.Terminals
+{
+    public interface ITerminalService
+    {
+
+    }
+
+    public class TerminalService : ITerminalService
+    {
+
+    }
+}
